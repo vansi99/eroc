1ae7cc63c00afb2b41189ffac9786422a134f52a
dev.portal <dev.portal@dichung.vn>
2020-07-08 07:34:45 +0000

    add separate

--- utils/string.js
+++ utils/string.js
@@ -4,5 +4,9 @@ string.removeViAccent = (content) => {
     return content.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/đ/g, 'd').replace(/Đ/g, 'D')
 }
 
+string.separate = (value) => {
+    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
 
 module.exports = string
