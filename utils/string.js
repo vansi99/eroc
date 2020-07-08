e1ba8c69d4ce04627cb1994e2c743be60be034c3
dev.portal <dev.portal@dichung.vn>
2020-07-08 07:39:37 +0000

    fix string separate

--- utils/string.js
+++ utils/string.js
@@ -4,8 +4,8 @@ string.removeViAccent = (content) => {
     return content.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/đ/g, 'd').replace(/Đ/g, 'D')
 }
 
-string.separate = (value) => {
-    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+string.separate = (value='') => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
 
