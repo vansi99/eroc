a174785cb5a1c442447e07e47748c4c5bbe0d78f
dev.portal <dev.portal@dichung.vn>
2020-10-26 08:16:26 +0000

    add counter

---
+++ counter.js
@@ -0,0 +1,41 @@
+const mongoose = require('eroc').mongoose
+
+
+const counter = {}
+const Schema = mongoose.Schema
+
+const schema = new Schema(
+    {
+        key: String,
+        value: Number,
+    },
+    {
+        timestamps: true
+    },
+)
+
+schema.index({ key: 1 })
+
+counter.Counter = mongoose.model('Counter', schema)
+
+counter.get = async (key) => {
+    const c = await counter.Counter.findOneAndUpdate(
+        {
+            key,
+        },
+        {
+            $inc: {
+                value: 1,
+            },
+        },
+        {
+            new: true,
+            upsert: true,
+        }
+    )
+
+    return c.value
+}
+
+
+module.exports = counter
