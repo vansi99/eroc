36236962e7549221d88bff27b0c5a339ef921c71
dev.portal <dev.portal@dichung.vn>
2020-12-16 11:01:56 +0000

    add eroc.query

--- index.js
+++ index.js
@@ -181,5 +181,24 @@ eroc.ui = (template, command={}) => {
     return router
 }
 
+eroc.query = () => {
+    const router = eroc.Router({ caseSensitive: true })
+    
+    router.post('/:model', async (req, res, next) => {
+        const model = req.params.model 
+        const pipeline = req.body
+    
+        let result = eroc.mongoose.models[model]
+    
+        Object.keys(pipeline).forEach((k) => {
+            result = Array.isArray(pipeline[k]) ? result[k](...pipeline[k]) : result[k](pipeline[k])
+        })
+        
+        return res.success(await result)
+    })
+
+    return router
+}
+
 
 module.exports = eroc
