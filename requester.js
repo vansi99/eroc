d5a9344c51fd920981606a02d34403b6042daadc
dev.portal <dev.portal@dichung.vn>
2020-12-21 10:55:12 +0000

    add client requester

--- requester.js
+++ requester.js
@@ -33,6 +33,32 @@ requester.fetch = ({ url, method, body, param, option }) => {
         }
     }
 
+
+
+    if (url.indexOf('http') !== 0) {
+
+        // handle internal service call. Exp: $user:v1/users
+        if (url.indexOf('$') === 0) {
+            const service = url.split(':')[0].slice(1)
+            const endpoint = url.slice(service.length + 2)
+
+            url = `http://${setting.apiBase || service}:3000/api/${service}/${endpoint}`
+
+            if (param) {
+                param.client = config.client
+            }
+
+            if (body) {
+                body.client = config.client
+            }
+        } else {
+
+            // add base api backend
+            url = `${setting.apiBase}/${url.replace(/^\/|\/$/g, '')}`
+        }
+
+    }
+
     if (body) {
         if (option.formData) {
             delete arg.headers['Content-Type']
@@ -43,29 +69,11 @@ requester.fetch = ({ url, method, body, param, option }) => {
         }
     }
 
-    if (url.indexOf('http') !== 0) {
-
-        // handle internal service call. Exp: $user:v1/users
-        if (url.indexOf('$') === 0) {
-            const service = url.split(':')[0].slice(1)
-            const endpoint = url.slice(service.length + 2)
-
-            url = `http://${setting.apiBase || service}:3000/api/${service}/${endpoint}`
-        } else {
-
-            // add base api backend
-            url = `${setting.apiBase}/${url.replace(/^\/|\/$/g, '')}`
-        }
-
-    }
-
     if (param) {
         url += (url.indexOf('?') !== 0? '?' : '&') + Object.keys({
             client: config.client,
             ...param,
         }).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(param[k])}`).join('&')
-
-
     }
 
     return fetch(url, arg).then((res) => {
