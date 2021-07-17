f7bc02f65f2bfa505e4150c2119f339e41474b16
shovity <shovity@hub_development_1>
2021-07-17 09:56:17 +0000

    add filter place  refine

--- utils/place.js
+++ utils/place.js
@@ -7,6 +7,16 @@ util.refine = (place) => {
     }
 
     if (place.address_components) {
+
+        place.address_components = place.address_components.filter((c) => {
+            
+            if (c.types.includes('postal_code')) {
+                return
+            }
+
+            return true
+        })
+
         place.components = place.address_components
         .map((c) => {
             return c.long_name.replace(/tỉnh|thành phố|tp\.|quận|huyện|phường|xã/gi, '').trim()
