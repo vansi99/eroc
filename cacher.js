1d6f3bc05e2591f09a8ed5c9c694d2e62c411ec4
shovity <shovity@hub_development_1>
2021-12-22 04:49:04 +0000

    update cacher key

--- cacher.js
+++ cacher.js
@@ -15,7 +15,7 @@ cacher.middle = (option) => {
     return async (req, res, next) => {
         const md5sum = crypto.createHash('md5')
         const [ base, param ] = req.originalUrl.split('?')
-        const key = `cacher:middle:${base}:${md5sum.update(param).digest('base64')}`
+        const key = `cacher:middle:${base}:${param ? md5sum.update(param).digest('base64') : ''}`
 
         if (req.headers.cacher !== 'disable') {
             const data = await rediser.get(key)
