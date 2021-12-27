115c7539781e04bda5a491896bdbcb91214667ba
shovity <shovity@hub_development_1>
2021-12-27 07:18:09 +0000

    fix prefix undefined

--- cacher.js
+++ cacher.js
@@ -13,14 +13,14 @@ cacher.middle = (option) => {
         prefix: '',
     }, option)
 
-    if (typeof prefix === 'function') {
-        prefix = prefix()
+    if (typeof option.prefix === 'function') {
+        option.prefix = option.prefix()
     }
 
     return async (req, res, next) => {
         const md5sum = crypto.createHash('md5')
         const [ base, param ] = req.originalUrl.split('?')
-        const key = `cacher:middle:${prefix}:${base}:${param ? md5sum.update(param).digest('base64') : ''}`
+        const key = `cacher:middle:${option.prefix}:${base}:${param ? md5sum.update(param).digest('base64') : ''}`
 
         if (req.headers.cacher !== 'disable') {
             const data = await rediser.get(key)
