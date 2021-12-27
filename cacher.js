e6ed8966356d2eab960aed1e810942357ddbecff
shovity <shovity@hub_development_1>
2021-12-27 07:09:30 +0000

    upgrade cacher.middle

--- cacher.js
+++ cacher.js
@@ -10,20 +10,29 @@ cacher.middle = (option) => {
     // default option
     option = Object.assign({
         expire: 173200, // 3d
+        prefix: '',
     }, option)
+
+    if (typeof prefix === 'function') {
+        prefix = prefix()
+    }
 
     return async (req, res, next) => {
         const md5sum = crypto.createHash('md5')
         const [ base, param ] = req.originalUrl.split('?')
-        const key = `cacher:middle:${base}:${param ? md5sum.update(param).digest('base64') : ''}`
+        const key = `cacher:middle:${prefix}:${base}:${param ? md5sum.update(param).digest('base64') : ''}`
 
         if (req.headers.cacher !== 'disable') {
             const data = await rediser.get(key)
 
             if (data) {
-                res.append('cacher', 'cached')
+                res.append('cacher', 'hit')
                 return res.json(data)
             }
+
+            res.append('cacher', 'miss')
+        } else {
+            res.append('cacher', 'disable')
         }
 
         res.u.listen('success', (data) => {
