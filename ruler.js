5ded7a68e4c1b47a7bf7f2cd09aed0f2c401d71b
vansi99 <vansido99@gmail.com>
2022-02-05 12:57:28 +0000

    change ruler

--- ruler.js
+++ ruler.js
@@ -29,69 +29,7 @@ ruler.buidPermisisons = (user) => {
     })
 }
 
-ruler.gate = (option={}) => {
-    const rediser = require('./rediser')
 
-    return (req, res, next) => {
-
-        const handle = async () => {
-            const token = req.headers.token || req.cookies.token
-
-            if (!token) {
-                if (option.weak) {
-                    return next()
-                }
-
-                if (option.api) {
-                    return res.error({ message: '401 Unauthorized' }, { code: 401 })
-                } else {
-                    res.u.cookie('token', '')
-                    return res.redirect(`/login?next=${req.originalUrl}`)
-                }
-            }
-
-            req.u.user = await jwt.verify(token).catch((error) => {
-                res.u.cookie('token', '')
-                return next(error)
-            })
-
-            const tiat = await rediser.hget('user_tiat', req.u.user._id)
-
-            if (tiat === null) {
-                if (option.api) {
-                    return res.error('user tiat not found')
-                } else {
-                    res.u.cookie('token', '')
-                    return res.redirect(`/login?next=${req.originalUrl}`)
-                }
-            }
-
-            if (req.u.user.iat < tiat) {
-                const { data } = await requester.get(`$user:v1/users/token`, { token })
-                req.u.user = await jwt.verify(data.token)
-
-                if (req.headers.token) {
-                    return res.error({
-                        message: 'token_expired',
-                        token: data.token,
-                    })
-                } else {
-                    res.u.cookie('token', data.token)
-                    req.cookies.token = data.token
-                    req.headers.cookie = req.headers.cookie.replace(`token=${token}`, `token=${data.token}`)
-                }
-            }
-
-            next()
-        }
-
-        handle().catch((error) => {
-            res.u.cookie('token', '')
-            console.error(error)
-            return next('Có lỗi trong quá trình đang nhập, vui lòng thử lại')
-        })
-    }
-}
 
 ruler.detect = () => {
     return async (req, res, next) => {
@@ -101,79 +39,16 @@ ruler.detect = () => {
 
             if (token) {
                 req.u.user = await jwt.verify(token).catch((error) => {
-                    res.u.cookie('token', '')
+                    res.cookie('token', '')
                     return next(error)
                 })
-            }
-    
-            if (!config.rulerDisableDetectClient && req.headers.client) {
-                let user = {}
-
-                if (config.clients) {
-                    user = {
-                        client: config.clients.find(c => c.key === req.headers.client)
-                    }
-                }
-
-                if (!user?.client) {
-                    const response = await requester.post('$user:in/query/User', {
-                        findOne: {
-                            'client.key': req.headers.client,
-                            status: 'active',
-                        },
-                        select: {
-                            client: 1,
-                        }
-                    })
-
-                    user = response.data
-                }
-
-                if (!user?.client) {
-                    throw 'client key not found'
-                }
-
-                req.u.client = user.client
-                req.u.client.uid = user._id
-            }
-
-            if (!config.rulerDisableDetectPartner && req.headers.partner) {
-                let user = {}
-
-                if (config.clients) {
-                    user = {
-                        client: config.clients.find(c => c.name === req.headers.name)
-                    }
-                }
-
-                if (!user?.client) {
-                    const response = await requester.post('$user:in/query/User', {
-                        findOne: {
-                            'client.name': req.headers.partner,
-                            status: 'active',
-                        },
-                        select: {
-                            client: 1,
-                        }
-                    })
-
-                    user = response.data
-                }
-
-                if (!user?.client) {
-                    throw 'partner name not found'
-                }
-
-                req.u.client = user.client
-                req.u.client.uid = user._id
             }
 
             next()
         }
 
         handle().catch((error) => {
-            res.u.cookie('token', '')
-            console.error(error)
+            res.cookie('token', '')
             return next('Có lỗi trong quá trình đang nhập, vui lòng thử lại')
         })
     }
